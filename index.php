cef0482bfad03d81db6bf09e472f6fdd4c10556c
David Martinez <davidmgilo@gmail.com>
2016-09-27 17:24:26 +0200

    sayhello namespace

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
+use davidmgilo\SayHello;
+
 //trobarà qualsevol classe del projecte
 require 'vendor/autoload.php';
+
+require 'src/SayHello.php';
 
 //$sayhello = new \HelloWorld\SayHello();
 //
 //echo $sayhello->world();
 
 echo \HelloWorld\SayHello::world();
+
+echo SayHello::world();
