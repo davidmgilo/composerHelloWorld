db8b02ee013d8549d87658fd7aa18250a30453cd
David Martinez <davidmgilo@gmail.com>
2016-09-27 17:04:53 +0200

    sayhello

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+//trobarà qualsevol classe del projecte
+require 'vendor/autoload.php';
+
+//$sayhello = new \HelloWorld\SayHello();
+//
+//echo $sayhello->world();
+
+echo \HelloWorld\SayHello::world();
